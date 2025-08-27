67e3033c2f8cdeee9a106e3a75fd18c11cf17271
Enrico Pistolesi <epi155.dev@gmail.com>
2025-08-27 12:07:43 +0200

    +javadoc on SqlCursor

--- src/main/java/io/github/epi155/emsql/runtime/SqlCursor.java
+++ src/main/java/io/github/epi155/emsql/runtime/SqlCursor.java
@@ -4,9 +4,35 @@ import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * Class to manage an SQL cursor and retrieve rows from a {@link java.sql.ResultSet ResultSet} one at a time
+ *
+ * @param <T> ResultSet data type
+ */
 public abstract class SqlCursor<T> implements AutoCloseable, Iterable<T> {
+    /**
+     * Returns {@code true} if the cursor has more elements.
+     *
+     * (In other words, returns {@code true} if {@link #fetchNext()} would return an element rather than throwing an exception.)
+     *
+     * @return {@code true} if the cursor has more elements
+     * @throws SQLException on SQL Error
+     */
     public abstract boolean hasNext() throws SQLException;
+
+    /**
+     * Returns the next element fetched from the cursor.
+     *
+     * @return next element fetched from the cursor
+     * @throws SQLException on SQL Error
+     */
     public abstract T fetchNext() throws SQLException;
+
+    /**
+     * Close the cursor.
+     *
+     * @throws SQLException on SQL Error
+     */
     @Override
     public abstract void close() throws SQLException;
 
